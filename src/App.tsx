176e792bc1492d1aafc8072f59668e0dad64c0c5
bassga <74778659+bassga@users.noreply.github.com>
2022-08-24 01:57:14 +0900

    remove sample (#2)
    test github actions

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>build by github Actions</p>
         <a
           className="App-link"
           href="https://reactjs.org"
