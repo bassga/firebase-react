77712735881f73f77e7636f8d84ad5f8c35e60a6
basega <bassmky19940208@gmail.com>
2022-08-24 01:35:27 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,13 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>build by github Actions</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        
+          Learn React
         </a>
       </header>
     </div>
